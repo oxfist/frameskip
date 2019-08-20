b5bd1a66c2c23f6f8d9f4c70f65ea3ba25e10585
Andrés Quilodrán <andres.caro.q@gmail.com>
2019-08-19 21:35:20 -0400

    Add Spring Boot as a dependency and main SpringApplication class

---
+++ src/main/java/com/quilodran/frameskip/FrameskipApplication.java
@@ -0,0 +1,12 @@
+package com.quilodran.frameskip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameskipApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FrameskipApplication.class, args);
+        System.out.println("Hello, Frameskip!");
+    }
+}
