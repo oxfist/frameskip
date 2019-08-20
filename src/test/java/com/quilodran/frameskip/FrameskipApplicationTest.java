137cdc9f237969aceca3d5b548ee23b5bd615976
Andrés Quilodrán <andres.caro.q@gmail.com>
2019-08-19 22:19:12 -0400

    Add test for context loading

---
+++ src/test/java/com/quilodran/frameskip/FrameskipApplicationTest.java
@@ -0,0 +1,12 @@
+package com.quilodran.frameskip;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FrameskipApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+
+}
