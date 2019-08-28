33457757c304852c4b81e6873d37490779832be3
Andrés Quilodrán <andres.c@thoughtworks.com>
2019-08-28 11:33:43 -0400

    Fix failing test and exclude JUnit 4 from Spring Boot Starter Test dependency

--- src/test/java/com/quilodran/frameskip/controller/QuoteControllerTest.java
+++ src/test/java/com/quilodran/frameskip/controller/QuoteControllerTest.java
@@ -2,22 +2,24 @@ package com.quilodran.frameskip.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-public class QuoteControllerTest {
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(QuoteController.class)
+class QuoteControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Test
     @DisplayName("GET /quote - Success")
-    public void should_return_solid_snake_quote() throws Exception {
+    void should_return_solid_snake_quote() throws Exception {
         mvc.perform(get("/quote"))
                 .andExpect(status().isOk());
     }
